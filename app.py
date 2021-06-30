f4b2c06dbaf8b22bc69ab02adfd51d6f292cd582
Heri Susanto Arisman <69309194+herisusantoarismann@users.noreply.github.com>
2021-06-30 09:17:01 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+# import Library
+from flask import Flask, render_template, request, redirect
+from googletrans import Translator
+from kbbi import KBBI, TidakDitemukan, AutentikasiKBBI
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# inisialisasi Flask
+app = Flask(__name__)
+translator = Translator()
+auth = AutentikasiKBBI('eriparigi18@gmail.com', 'heris783')
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+def terjemah(kalimat):
+    terjemahan = []
+    translation = translator.translate(kalimat, src="id", dest="en")
+    terjemahan = translation.text
+
+    return terjemahan
+
+def kbbi(kata):
+    try:
+        hasil = KBBI(kata, auth)
+        hasil = hasil.__str__(contoh=False, terkait=False)
+    except TidakDitemukan as e:
+        hasil = e.objek
+        hasil = {"hasil":hasil.saran_entri}
+
+    return hasil
+
+def stemming(kata):
+    kata = stemmer.stem(kata)
+
+    return kata
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    kalimat = request.form['kalimat']
+    hasil_translate = terjemah(kalimat)
+    return hasil_translate
+
+@app.route('/kbbi', methods=['POST'])
+def check():
+    kata = request.form['kata']
+    kata_stem = stemming(kata)
+    arti = kbbi(kata_stem)
+    return arti
+
+if __name__ == "__main__":
+    app.run(debug = True)
